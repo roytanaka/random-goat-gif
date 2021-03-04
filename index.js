9800125f4170ae4d9012273aaff311d8c925b72c
Roy Tanaka <tanaka@mac.com>
2021-03-04 15:31:35 -0500

    no video open graph

--- index.js
+++ index.js
@@ -16,14 +16,13 @@ app.use('/', async (req, res) => {
       `https://api.giphy.com/v1/gifs/random?api_key=4x1BLBVGj8byXpfHMhkh3tx9RJc17xJo&tag=goats&rating=g`
     );
     const data = giphyRes.data.data;
-    const { mp4, url, height, width } = data.images.fixed_width;
+    const { url, height, width } = data.images.downsized_large;
     res.render('index', {
       imageGiphy: data.url,
       imageDescription: data.title,
       imageUrl: url,
       imageWidth: width,
       imageHeight: height,
-      videoUrl: mp4,
     });
   } catch (error) {
     console.error('/ error:', error);
